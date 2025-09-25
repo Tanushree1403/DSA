70b9e7e483d9d07cfad2f8906ffcd52b1ae863f3
Tanushree <shreetanu979@gmail.com>
2025-09-24 20:30:38 -0700

    Reverse a Binary tree

---
+++ Binary Tree/ReverseBTree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {TreeNode}
+     */
+    invertTree(root) {
+        if( root == null) return null;
+
+        var tmpNode = root.left;
+        root.left = root.right;
+        root.right = tmpNode;
+
+        this.invertTree(root.left);
+        this.invertTree(root.right);
+
+        return root;
+
+    }
+}
